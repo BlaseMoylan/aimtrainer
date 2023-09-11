6379c3cb093095324685440431151c8003c71ac2
Blase Moylan <moylanblase@gmail.com>
2023-09-11 11:26:13 -0700

    📦 NEW: frame rate and targets displayed

--- tutorial.py
+++ tutorial.py
@@ -9,6 +9,13 @@ WIDTH, HEIGHT = 800, 600
 WIN =pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Aim Trainer")
 
+TARGET_INCREMENT =400
+TARGET_EVENT = pygame.USEREVENT
+
+TARGET_PADDING=30
+
+BG_COLOR = (0, 25, 40)
+
 class Target:
     MAX_SIZE =30
     GROWTH_RATE=0.2
@@ -23,9 +30,9 @@ class Target:
 
     def update(self):
         if self.size +self.GROWTH_RATE >= self.MAX_SIZE:
-            self.grow= False
+            self.growth= False
 
-        if self.grow:
+        if self.growth:
             self.size +=self.GROWTH_RATE
         else:
             self.size -= self.GROWTH_RATE
@@ -36,14 +43,42 @@ class Target:
         pygame.draw.circle(win, self.COLOR, (self.x, self.y), self.size * 0.6)
         pygame.draw.circle(win, self.SECOND_COLOR, (self.x, self.y), self.size * 0.4)
 
+def draw(win,targets):
+    win.fill(BG_COLOR)
+
+    for target in targets:
+        target.draw(win)
+
+    pygame.display.update()
+
 def main():
     run= True
+    targets = []
+    clock = pygame.time.Clock()
+
+    pygame.time.set_timer(TARGET_EVENT, TARGET_INCREMENT)
 
     while run:
+        clock.tick(60)
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run= False
                 break
+
+            if event.type== TARGET_EVENT:
+                x=random.randint(TARGET_PADDING, WIDTH - TARGET_PADDING)
+                y=random.randint(TARGET_PADDING,HEIGHT-TARGET_PADDING)
+                target=Target(x,y)
+                targets.append(target)
+        
+        for target in targets:
+            target.update()
+
+            if target.size <= 0:
+                targets.remove(target)
+
+        draw(WIN, targets)
 
     pygame.quit()
 
