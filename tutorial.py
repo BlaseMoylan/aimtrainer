7a43349ac016b8aac8e55aaa284c46744e1ff4ee
Blase Moylan <moylanblase@gmail.com>
2023-09-11 15:56:32 -0700

    📦 NEW: added logic for handling the interaction between mouse and target

--- tutorial.py
+++ tutorial.py
@@ -43,6 +43,10 @@ class Target:
         pygame.draw.circle(win, self.COLOR, (self.x, self.y), self.size * 0.6)
         pygame.draw.circle(win, self.SECOND_COLOR, (self.x, self.y), self.size * 0.4)
 
+    def collide(self, x, y):
+        dis = math.sqrt((self.x-x)**2 + (self.y-y)**2)
+        return dis <= self.size
+
 def draw(win,targets):
     win.fill(BG_COLOR)
 
@@ -56,10 +60,17 @@ def main():
     targets = []
     clock = pygame.time.Clock()
 
+    targets_pressed = 0
+    clicks = 0
+    misses = 0
+    start_time = time.time()
+
     pygame.time.set_timer(TARGET_EVENT, TARGET_INCREMENT)
 
     while run:
         clock.tick(60)
+        click = False
+        mouse_pos = pygame.mouse.get_pos()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -71,12 +82,23 @@ def main():
                 y=random.randint(TARGET_PADDING,HEIGHT-TARGET_PADDING)
                 target=Target(x,y)
                 targets.append(target)
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                click = True
+                clicks += 1
         
         for target in targets:
             target.update()
 
             if target.size <= 0:
                 targets.remove(target)
+                misses += 1
+
+            if click and target.collide(*mouse_pos):
+                targets.remove(target)
+                targets_pressed += 1
+
+
 
         draw(WIN, targets)
 
